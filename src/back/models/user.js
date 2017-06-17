f0705661c05fa17995f1556c17cacf65423023b9
Ruben Gil <gil.r419@gmail.com>
2017-06-17 13:58:54 -0400

    Create user model

---
+++ src/back/models/user.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define('User', {
+    email: {
+      type: DataTypes.STRING
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8, 26]
+      }
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        length: [1, 30],
+        isAlpha: true
+      }
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        length: [1, 30],
+        isAlpha: true
+      }
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return User;
+};
