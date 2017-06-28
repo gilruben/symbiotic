af139f721c453eb599afa201e9c3892936f43969
Ruben Gil <gil.r419@gmail.com>
2017-06-28 18:40:07 -0400

    Add api route to server

--- src/back/server.js
+++ src/back/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const path = require('path');
 const db = require('./models');
+const router = require('./routes');
+const applyExpressMiddleware = require('./middleware');
 
 const app = express();
+
+// Adds middleware to express
+applyExpressMiddleware(app);
+
+// API route
+app.use('/api', router);
+
 
 db.sequelize.sync().then(() => {
   const port = process.env.PORT || 5555;
