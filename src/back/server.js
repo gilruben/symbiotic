28ed5755d3542880e76ae3410cbacf28d2e62a50
Ruben Gil <gil.r419@gmail.com>
2017-06-30 20:15:20 -0400

    Connect auth routes to server

--- src/back/server.js
+++ src/back/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const db = require('./models');
 const router = require('./routes');
+const authRouter = require('./routes/auth');
 const applyExpressMiddleware = require('./middleware');
 
 const app = express();
@@ -11,6 +12,9 @@ applyExpressMiddleware(app);
 
 // API route
 app.use('/api', router);
+
+// Auth route
+app.use('/auth', authRouter);
 
 
 db.sequelize.sync().then(() => {
