248b1560ef6003d181121e0e40417cca7501d187
Ruben Gil <gil.r419@gmail.com>
2017-06-30 20:13:21 -0400

    Create login auth route

---
+++ src/back/routes/auth.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const User = require('../models').user;
+const sign = require('../passport/sign-token');
+
+const authenticate = require('passport').authenticate('jwt', { session: false });
+
+const authRouter = () => {
+  const userLogin = (req, res) => {
+    const { email, password } = req.body;
+    const loginData = { email, password };
+
+    User.findOne({
+      where: loginData,
+      attributes: {
+        exclude: ['password']
+      }
+    })
+    .then((user) => {
+      console.log(user);
+      if (user) {
+        req.session.jwt = sign({ id: user.id });
+        req.session.save();
+
+        res.send(user);
+      } else {
+        res.sendStatus(401);
+      }
+    })
+    .catch(() => {
+      res.sendStatus(401);
+    });
+  };
+
+
+  router.route('/login')
+    .post(userLogin);
+
+
+  return router;
+}
+
+module.exports = authRouter();
